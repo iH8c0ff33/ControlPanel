1be43d2608b824655941b31fe90bad6558f4caa0
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2015-03-24 18:46:21 +0100

    fixed voltage read in PSU

--- PSU/PSU.ino
+++ PSU/PSU.ino
@@ -22,6 +22,7 @@ const char VOL1off	= 'l';
 boolean read0 = 0;
 boolean read1 = 0;
 int Voltage = 0;
+char voltage[3] = 0;
 double Vcc;
 const double R1 = 1005000;
 const double R2 = 9950;
@@ -53,13 +54,19 @@ void loop() {
 	if (read0) {
 		Wire.beginTransmission(1);
 		Voltage = ((analogRead(A0) / 1023.0) * Vcc) * (R2 / (R1 + R2));
-		Wire.write(Voltage);
+		voltage[] = {Voltage / 100 % 10, Voltage / 10 % 10, Voltage % 10};
+		Wire.write(voltage[0]);
+		Wire.write(voltage[1]);
+		Wire.write(voltage[2]);
 		Wire.write(";");
 		Wire.endTransmission();
 	} else if (read1) {
 		Wire.beginTransmission(1);
 		Voltage = ((analogRead(A1) / 1023.0) * Vcc) * (R2 / (R1 + R2));
-		Wire.write(Voltage);
+		voltage[] = {Voltage / 100 % 10, Voltage / 10 % 10, Voltage % 10};
+		Wire.write(voltage[0]);
+		Wire.write(voltage[1]);
+		Wire.write(voltage[2]);
 		Wire.write(";");
 		Wire.endTransmission();
 	}
